77adb744cf21fd2fb942f6c228030259cb513d1c
koszmarr <miroslawduplaga1992@gmail.com>
2025-08-20 21:16:27 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-// Simple SW – cache static assets
-const CACHE = 'sxbet-ladder-v1';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
-self.addEventListener('install', (e)=>{
+// Minimalny service worker – cache statyczny
+const CACHE = 'sxbet-pwa-v1';
+const ASSETS = ['/', './', './index.html', './manifest.webmanifest'];
+
+self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', (e)=>{
+self.addEventListener('activate', e=>{
   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
 });
 
-self.addEventListener('fetch', (e)=>{
+self.addEventListener('fetch', e=>{
   const url = new URL(e.request.url);
-  if(url.origin.includes('api.sx.bet')) return;
-  e.respondWith(caches.match(e.request).then(resp=>resp||fetch(e.request)));
+  if (url.origin === location.origin){
+    e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)));
+  }
 });
